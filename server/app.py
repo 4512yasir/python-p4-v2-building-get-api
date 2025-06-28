4c313fbcc92a9ec3317c0749f7124f743fbdd6d7
4512yasir <yasiryaska1@gmail.com>
2025-06-28 16:20:20 +0300

    building api

--- server/app.py
+++ server/app.py
@@ -21,7 +21,35 @@ def index():
 
 # start building your API here
 
+# server/app.py
+@app.route('/games')
+def get_games():
+    games = [game.to_dict() for game in Game.query.all()]
+    response = make_response(games, 200)
+    return response
+
+@app.route('/games/<int:id>')
+def get_game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if game:
+        response = make_response(game.to_dict(), 200)
+    else:
+        response = make_response({'error': 'Game not found'}, 404)
+    return response
+
+
+@app.route('/games/users/<int:id>')
+def get_game_users(id):
+    game = Game.query.filter(Game.id == id).first()
+    if game:
+        users = [user.to_dict(rules=('-reviews',)) for user in game.users]
+        response = make_response(users, 200)
+    else:
+        response = make_response({'error': 'Game not found'}, 404)
+    return response
+
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5556, debug=True)
 
